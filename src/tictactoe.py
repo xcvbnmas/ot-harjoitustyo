e748de92c8a2e823f2965a5710406b4c50e47296
xcvbnmas <132822281+xcvbnmas@users.noreply.github.com>
2023-11-28 20:35:49 +0200

    viikko4

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,31 +1,83 @@
 import tkinter as tk
+from tkinter import messagebox
 
-class TicTacToe:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Ristinolla")        
-        self.root.resizable(False, False)
-        self.buttons = self.create_grid()
+class TicTacToeGame(tk.Tk):
+    def __init__(self):
+        self.grid = [' '] * 9
+        self.current = 'X'
+        
+        
+    def play(self, i, j):
+        if self.grid[i * 3 + j] == ' ':
+            self.grid[i * 3 + j] = self.current
+            return True
+        return False
 
+    def winner(self):
+        # tähän on käytetty apuna ChatGPT
+        for i in range(3):
+            if self.grid[i * 3] == self.grid[i * 3 +1] == self.grid[i * 3 + 2] != ' ':
+                return True
+            if self.grid[i] == self.grid[i + 3] == self.grid[i + 6] != ' ':
+                return True
+        if self.grid[0] == self.grid[4] == self.grid[8] != ' ' or \
+            self.grid[2] == self.grid[4] == self.grid[6] != ' ':
+            return True    
+        return False
+        # tähän loppuu koodi, johon käytetty apuna ChatGPT 
+        
+    def switch_player(self):
+        if self.current == 'X':
+            self.current = 'O'
+        else:
+            self.current = 'X'
+            
+    def reset_game(self):
+        self.grid = [' '] * 9
+        self.current = 'X'
+
+class TicTacToeGrid(tk.Tk):
+    def __init__(self, game):
+        super().__init__()
+        self.title("Ristinolla")
+        self.create_grid()
+        self.game = game
+        self.resizable(False, False)       
+        
     def create_grid(self):
-        buttons = []
-        for i in range(3):               
-            row = []            
+        self.buttons = []
+        for i in range(3):
+            row = []
             for j in range(3):
-                button = tk.Button(self.root, font=('Arial', 20), width=10, height=5, command=lambda i=i, j=j: self.on_click(i, j))
+                button = tk.Button(self, font=('Arial', 20), width=10, height=5, command=lambda i=i, j=j: self.click_button(i, j))
                 button.grid(row=i, column=j, sticky='nsew')
                 row.append(button)
-                           
-            buttons.append(row)
-            
-            self.root.grid_rowconfigure(i, minsize=50)
-            self.root.grid_columnconfigure(j, minsize=50)         
-            
-        return buttons
-            
+            self.buttons.append(row)
 
+        for i in range(3):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1)
+            
+    def click_button(self, i, j):
+        if self.game.play(i, j):
+            self.buttons[i][j].config(text=self.game.current, state='disabled')
+            if self.game.winner():
+                messagebox.showinfo("Ristinolla", f"{self.game.current} voittaa")
+                self.game.reset_game()
+                self.reset_grid()
+            elif ' ' not in self.game.grid:
+                messagebox.showinfo("Ristinolla", "Tasapeli")
+                self.game.reset_game()
+                self.reset_grid()
+            else:
+                self.game.switch_player()
+	        
+    def reset_grid(self):
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].config(text=' ', state='normal')
+    
 if __name__ == "__main__":
-    root = tk.Tk()    
-    game = TicTacToe(root)
-    root.mainloop()
-                   
+    game = TicTacToeGame()
+    app = TicTacToeGrid(game)
+    app.mainloop()
--- tasks.py
+++ tasks.py
@@ -3,12 +3,10 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run("python3 src/index.py", pty=True)
+    ctx.run("python3 src/tictactoe.py", pty=True)
 
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
     
-@task(coverage)
-def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+

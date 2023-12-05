0be83d4cbab228a819a290e7db586a29b866b127
xcvbnmas <132822281+xcvbnmas@users.noreply.github.com>
2023-12-05 19:22:59 +0200

    viikko5

--- tasks.py
+++ tasks.py
@@ -11,7 +11,7 @@ def test(ctx):
     
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task()
 def coverage_report(ctx):
